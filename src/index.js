1f5ec3dac01957ccf45e0287271c0392493ec785
Ray Lee <rhlee@berkeley.edu>
2016-11-29 21:52:32 -0800

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import styles from '../styles/cspace-ui-theme/sample.css';
+
+module.exports = () => ({
+  className: styles.themed,
+});
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+/* eslint import/no-extraneous-dependencies: "off" */
+
+const webpack = require('webpack');
+
+const library = 'cspaceUIPluginThemeSample';
+const isProduction = process.env.NODE_ENV === 'production';
+const filename = `${library}${isProduction ? '.min' : ''}.js`;
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename,
+    library,
+    libraryTarget: 'umd',
+    path: 'dist',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&localIdentName=[folder]-[name]--[local]',
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+};
+
+if (isProduction) {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
+module.exports = config;
